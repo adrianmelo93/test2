cec5d9d085a50494f8d3b8c884fcd3529d572261
fvi computer <t@etest.com>
2016-12-21 18:08:53 -0500

    copy files from WEB1010

---
+++ Q13.js
@@ -0,0 +1,7 @@
+// Write a for loop that prints to the console all the numbers from 1 to 10 in
+// ascending order.
+
+for (var a = 1; a <= 10; a++){
+console.log(a);
+
+}
---
+++ Q4.js
@@ -0,0 +1,4 @@
+// Log to the console any expression that uses the modulo operator % and results
+// in the number 1.
+var resul = 5 % 2;
+console.log(resul);
